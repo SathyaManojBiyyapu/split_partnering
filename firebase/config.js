433c76e6aa0c8d988dd389741bb2b209b7a85b81
Sathya <sathyamanojbiyyapu@gmail.com>
2026-05-16 02:16:36 +0530

    Fixed Firebase initialization

--- firebase/config.js
+++ firebase/config.js
@@ -1,21 +1,45 @@
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp, getApps } from "firebase/app";
+
+import {
+  getAuth,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
 import { getFirestore } from "firebase/firestore";
+
+/* ---------------- FIREBASE CONFIG ---------------- */
 
 const firebaseConfig = {
   apiKey: "AIzaSyBx11nyHmS-xw1jyeDEX_Qxsd4cGhJqk18",
-  authDomain: "splitpartnering.firebaseapp.com",
+
+  authDomain:
+    "splitpartnering.firebaseapp.com",
+
   projectId: "splitpartnering",
-  storageBucket: "splitpartnering.firebasestorage.app",
+
+  storageBucket:
+    "splitpartnering.firebasestorage.app",
+
   messagingSenderId: "656417651216",
-  appId: "1:656417651216:web:cd20cd09120190171ea895",
+
+  appId:
+    "1:656417651216:web:cd20cd09120190171ea895",
+
   measurementId: "G-Z9LQZ49T2B",
 };
 
-const app = initializeApp(firebaseConfig);
+/* ---------------- PREVENT REINITIALIZATION ---------------- */
+
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
+
+/* ---------------- SERVICES ---------------- */
 
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
 
-/* ✅ THIS LINE WAS MISSING */
-export const googleProvider = new GoogleAuthProvider();
+export const googleProvider =
+  new GoogleAuthProvider();
